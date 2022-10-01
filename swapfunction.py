b1557e405d4fbc1b61eae71b0be03d9b62a70c85
ayush_0001 <ayushk0004@gmail.com>
2022-10-01 16:28:34 +0530

    first programme

---
+++ swapfunction.py
@@ -0,0 +1,9 @@
+#Take inputs from users for two numbers ( x and y)
+x=input("enter the first number")
+y=input("enter the second number")
+z=int
+z=x
+x=y
+y=z
+print("first number is ",x)
+print("second number is ",y)
