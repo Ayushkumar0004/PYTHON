5f23dc0554c3e5a5f06c027ae2b8e249e2e9a839
ayush_0001 <ayushk0004@gmail.com>
2022-10-05 12:03:31 +0530

    swapping

---
+++ swapfn.py
@@ -0,0 +1,11 @@
+#Take imputs from users for two numbers (x,y)
+
+x=float(input("enter the first number"))
+y=float(input("enter the second number"))
+
+z=int
+z=x
+x=y
+y=z
+print("first number is",x)
+print("second number is",y)
