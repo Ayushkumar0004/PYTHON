1966f4b59cf61465a95976401094a2f19bde149a
ayush_0001 <ayushk0004@gmail.com>
2022-10-09 16:19:18 +0530

    even or odd

---
+++ oddorevenno.py
@@ -0,0 +1,9 @@
+#Take the input from users
+#identify it is odd or even
+
+num=int(input("enter the number:"))
+
+if (num % 2) == 0:
+    print("the number is even:")
+else:
+    print("the number is odd:")    
