944345fddd816e688ec0dae9ad2f5ce8beb004ca
ayush_0001 <ayushk0004@gmail.com>
2022-10-09 13:16:26 +0530

    square or not a square

---
+++ squareornot.py
@@ -0,0 +1,11 @@
+#take values of length and breadth from user
+#print if it is a square or not
+
+length=int(input("enter the first number"))
+breadth=int(input("enter the second number"))
+
+if length==breadth:
+    print('it is a square')
+
+else:
+    print("it is not a square")
