bb52e3255ed904fa1e117a44c57666d3cd753d9f
ayush_0001 <ayushk0004@gmail.com>
2022-10-09 13:09:58 +0530

    IF ELSE

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+#Basic Operators
+#take user input for x and y
+#If x is less than y, print YAYY
+#else print NAYY
+
+x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+
+if x<y:
+    print("YAYY")
+
+else:
+    print("NAYY")
+
