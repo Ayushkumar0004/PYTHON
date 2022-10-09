abf04cbc5405aba25408d63b1610fdead53dddf9
ayush_0001 <ayushk0004@gmail.com>
2022-10-09 12:53:45 +0530

    distance

---
+++ milestokm.py
@@ -0,0 +1,8 @@
+#Take user input for miles
+#MILES TO KM
+
+distancesInMiles=int(input("Enter the distance in miles:"))
+
+distanceInKm=distancesInMiles*1.60934
+
+print("distance in km:",distanceInKm)
