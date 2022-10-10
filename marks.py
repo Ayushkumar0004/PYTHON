c4688b5cefc027e0f127494067477076c32ff722
ayush_0001 <ayushk0004@gmail.com>
2022-10-10 22:39:45 +0530

    scores

---
+++ marks.py
@@ -0,0 +1,13 @@
+marks=int(input("enter the marks:"))
+if marks<25:
+    print("f")
+elif marks>24 and marks<45:
+    print("E")
+elif marks>45 and marks<50:
+    print("D")
+elif marks > 50 and marks < 60:
+    print("D")
+elif marks > 60 and marks < 80:
+    print("C")               
+else:
+    print("A")    
