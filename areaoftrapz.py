c96e71ec48a5ae158cb48188b82fd898be154672
ayush_0001 <ayushk0004@gmail.com>
2022-10-12 23:43:02 +0530

    trapezium area

---
+++ areaoftrapz.py
@@ -0,0 +1,10 @@
+#area of trapezium
+
+base1=int(input("enter the base 1:"))
+base2=int(input("enter the base 2:"))
+height=int(input("enter the height:"))
+
+area=(base1 + base2)/2*height
+
+
+print("the area is:" ,area)
