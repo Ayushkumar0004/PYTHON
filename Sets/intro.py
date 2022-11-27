a1a8c844874651c8186f2c2f9ae056c9e081c2fe
ayush_0001 <ayushk0004@gmail.com>
2022-11-27 16:38:05 +0530

    Intro Sets

---
+++ Sets/intro.py
@@ -0,0 +1,15 @@
+#Set=store multiple values in single variable
+#unordered
+myset = {"car","boat","train"}
+print(myset)
+#duplicates are not allowed
+#unchangable/immutable
+# non homogenous  
+if "boat" in myset:
+    print("Yaa")
+else:
+    print("No")
+tuple1 = ("H","E","L","L","O")
+for i in tuple1:
+    print(i,end="")   
+     
