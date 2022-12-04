ea3a53373b9cf880221945fae0961be5ff777e69
ayush_0001 <ayushk0004@gmail.com>
2022-12-04 13:33:42 +0530

    Nested-dict

--- Dictionary/intro.py
+++ Dictionary/intro.py
@@ -1,6 +1,5 @@
 #Dictionary are ordered
 #key and value pair are must
-
 mydictionary = {
     "name":"Ayush",
     "Age":"18",
@@ -8,9 +7,17 @@ mydictionary = {
     "percentage":83
 }
 a = mydictionary.keys()
+b = mydictionary.values()
 print(a)
+print(b)
 print(mydictionary.get("Age"))
 print((mydictionary))
 print(len(mydictionary))
 #ordered 
 #duplicates are not allowed
+mydictionary["Age"] =19
+print(mydictionary)
+mydictionary.update({"Age":22})
+print(mydictionary)
+mydictionary.pop("Class")
+print(mydictionary)
---
+++ Dictionary/nested_dict.py
@@ -0,0 +1,12 @@
+myDictionary = {
+    "class":{
+        "student":{
+            "name" : "Ayush",
+            "marks" : {
+                "python" : 90,
+                "web" : 95
+            }
+        }
+    }
+}
+print(myDictionary["class"]["student"]["marks"]["web"])
