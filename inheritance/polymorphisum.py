ed9c64977c1895daa7c42edf278f19cda558e5c9
ayush_0001 <ayushk0004@gmail.com>
2022-12-18 16:34:32 +0530

    Polymerism

---
+++ inheritance/polymorphisum.py
@@ -0,0 +1,62 @@
+# a =  "Hello"
+# b = [1,2,3,4,5,6]
+# print(len(a))
+# print(len(b))
+
+
+# def add(a,b,c = 0):
+#     return a+b+c
+# print(add(1,2))
+# print(add(3,4,2))    
+
+
+# class Rect:
+#     def calculateArea(self,length=None,breadth=None):
+#         if length!=None and breadth!=None:
+#             return length * breadth
+#         elif length != None:
+#             return length*length    
+
+
+# rectangle = Rect()
+# print(rectangle.calculateArea(2,3))
+# print(rectangle.calculateArea(2))
+
+
+
+# class bird:
+#     def category(self):
+#         print("This is a category of bird..")
+#     def species(self):
+#         print("I am endagered species..")    
+# class sparrow(bird):
+#     pass
+# class crow(bird):
+#     pass
+# class ostrich(bird):
+#     pass
+# class Dodo(bird):
+#     pass
+# obj_Sparrow = sparrow()
+# objCrow = crow()
+# objCrow.category()
+# obj_Sparrow.category()
+# objDodo = Dodo()
+# objDodo.species()
+
+# class car1:
+#     def seating(self):
+#         print("The seating capacity id Four")
+# class car2:
+#     def seating(self):
+#         print("The seating capacity is two")
+# class bus:
+#     def seating(self):
+#         print("The seating capacity is 50")
+# Car1 = car1()
+# Car2 = car2()
+# bus1 = bus()  
+# Car1.seating()
+# Car2.seating()
+# bus1.seating()      
+
